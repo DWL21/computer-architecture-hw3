0791c1306f9c79e7adda94d0739e3b04fb936130
dwl21 <nggus5@gmail.com>
2025-06-05 11:09:56 +0900

    feat: remove quotes in all benchmarks

--- run_all_benchmarks.py
+++ run_all_benchmarks.py
@@ -4,10 +4,8 @@ benchmarks = ['-a', '-b', '-c', '-d']
 
 procs = []
 for bench in benchmarks:
-    # 각 벤치마크를 별도 프로세스로 실행
     proc = subprocess.Popen(['python3', 'run_benchmark.py', bench])
     procs.append(proc)
 
-# 모든 프로세스가 끝날 때까지 대기
 for proc in procs:
     proc.wait() 
