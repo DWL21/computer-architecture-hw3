f7d648f2629e82671682dd36666dc6f77ce41868
dwl21 <nggus5@gmail.com>
2025-06-05 07:07:12 +0000

    run benchmarks nohup

---
+++ run_benchmarks_nohup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# 모든 파이썬 프로세스 종료
+echo "Killing all running Python scripts..."
+pkill -f .py
+
+# nohup으로 run_all_benchmarks.py 실행
+echo "Starting run_all_benchmarks.py with nohup..."
+nohup python3 run_all_benchmarks.py > nohup.output 2>&1 &
+echo "Started. Output is being written to nohup.output." 
